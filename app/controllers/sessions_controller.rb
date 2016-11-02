2312335073a965d39f9b16a72c94de2b8d8313c7
yhormanP <yperez.gap@gmail.com>
2016-11-02 02:17:08 +0000

    Login validating gap domain

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,8 @@ class SessionsController < ApplicationController
   layout "login"
  
   def new
-    if current_user 
-      redirect_to dashboard_path      
+    if current_user
+      redirect_to dashboard_path
     end
   end
-
-end 
+end
